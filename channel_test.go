33eae06da4c720e0bb6b6352b40eb484dea31af7
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-06-04 22:03:59 +0700

    channel

---
+++ channel_test.go
@@ -0,0 +1,23 @@
+package belajar_golang_goroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Bayu Sugiantoro"
+		fmt.Println("Selesai Mengirim Data ke Channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
